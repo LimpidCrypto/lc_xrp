9756fa16a4941ca56fe561c80f9220c0930ede04
LimpidCrypto <konermann@limpidcrypto.com>
2024-03-07 13:02:31 +0000

    rename attach to apply

---
+++ bin/apply_all.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# apply namespaces
+kubectl apply -f ../k8s/namespace.yaml
+
+# apply secrets
+kubectl apply -f ../k8s/db/secret.yaml
+kubectl apply -f ../k8s/mailer/secret.yaml
+
+# apply configmaps
+
+# apply pv
+kubectl apply -f ../k8s/db/pv.yaml
+
+# apply pvc
+kubectl apply -f ../k8s/db/pvc.yaml
+
+# apply deployments
+kubectl apply -f ../k8s/app/deployment.yaml
+kubectl apply -f ../k8s/db/deployment.yaml
+kubectl apply -f ../k8s/mailer/deployment.yaml
+kubectl apply -f ../k8s/redis/deployment.yaml
+
+# apply services
+kubectl apply -f ../k8s/app/service.yaml
+kubectl apply -f ../k8s/db/service.yaml
+kubectl apply -f ../k8s/mailer/service.yaml
+kubectl apply -f ../k8s/redis/service.yaml
